141959b56598e7ea6f42a907dffab300f3461d80
ravik122 <ravik1222@gmail.com>
2012-08-16 11:49:19 +0200

    testing done, time to populate the database

---
+++ database/script.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#for i in {1..44}
+#do
+#	i=1
+#	name=`sed -n "$ip" sth2`
+#	echo "name: $name"
+#	status=`echo $name | cut -c -7`
+#	echo "status: $status"
+#	
+#	if [ "$status" = "[32;48m" ]; then
+#		number=`cat $name/$USER.start`
+#		sed -i "\$as_$number$name/" .database
+#	fi
+#
+#
+#	if [ "$status" = "[30;48m" ]; then
+#		sed -i "\$an__/$name/" .database
+#	fi
+#
+#	if [ "$status" = "[31;48m" ]; then
+#		sed -i "\$ad__/$name/" .database
+#	fi
+#done
+	lol=1
+	name=`sed -n "$lolp" sth2`
+	echo $name
